88296d593b0375b0737f17b16c13369aa1d3009b
hesam-init <hesam.init@gmail.com>
2024-02-14 21:49:38 +0330

    feat: random wallpaper on startup

--- hyprland/.config/hypr/scripts/swww-launch.sh
+++ hyprland/.config/hypr/scripts/swww-launch.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-source $HOME/.config/hypr/scripts/env.sh
-
 if [[ $(pidof swaybg) ]]; then
     pkill swaybg
 fi
 
 swww query || swww init
 
-swww img $wallpapers_dir/macos.jpg --transition-fps $swww_fps --transition-type any --transition-duration $swww_duration
+exec $HOME/.config/hypr/scripts/swww-random.sh
