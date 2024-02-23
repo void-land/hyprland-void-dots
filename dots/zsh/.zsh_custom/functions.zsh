3aeeae548a3e4536318c0f07fae35491772d672c
hesam-init <hesam.init@gmail.com>
2024-02-24 02:06:19 +0330

    feat: convert video to gif function

--- dots/zsh/.zsh_custom/functions.zsh
+++ dots/zsh/.zsh_custom/functions.zsh
@@ -1,1 +1,8 @@
+convert_video_to_gif() {
+    local input_video="$1"
+    local output_filename="output_$(date +"%Y%m%d%H%M%S").gif"
 
+    ffmpeg -i "$input_video" -vf "fps=30,scale=1920:-1:flags=lanczos" -c:v gif "$output_filename"
+
+    echo "GIF created: $output_filename"
+}
