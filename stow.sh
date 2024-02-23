f642f3f3d08b441da1a5cb90f833d0d0433378ff
hesam-init <hesam.init@gmail.com>
2024-02-23 23:55:14 +0330

    feat: function for stow and unstow

--- stow.sh
+++ stow.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+display_help() {
+    echo "Usage: [-s | -u] [-h]"
+    echo "  -s   Stow dotfiles"
+    echo "  -u   Unstow dotfiles"
+    echo "  -h   Display this help message"
+}
+
 stow_dotfiles() {
     stow dots
     stow -d dots -S zsh -t ~/
     stow hyprland
-
     echo "Dotfiles stowed successfully!"
 }
 
-stow_dotfiles
+unstow_dotfiles() {
+    stow -D dots
+    stow -D -d dots -t ~/ zsh
+    stow -D hyprland
+    echo "Dotfiles unstowed successfully!"
+}
+
+while getopts ":suh" opt; do
+    case $opt in
+    s)
+        stow_dotfiles
+        ;;
+    u)
+        unstow_dotfiles
+        ;;
+    *)
+        display_help
+        ;;
+    esac
+done
