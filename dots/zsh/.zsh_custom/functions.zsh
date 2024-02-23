5cc7e280c328076ae37ac3cf7d0989c78c468365
hesam-init <hesam.init@gmail.com>
2024-02-24 02:28:33 +0330

    chore: convert video to gif fps

--- dots/zsh/.zsh_custom/functions.zsh
+++ dots/zsh/.zsh_custom/functions.zsh
@@ -1,8 +1,9 @@
 convert_video_to_gif() {
+    local fps=60
     local input_video="$1"
     local output_filename="output_$(date +"%Y%m%d%H%M%S").gif"
 
-    ffmpeg -i "$input_video" -vf "fps=30,scale=1920:-1:flags=lanczos" -c:v gif "$output_filename"
+    ffmpeg -i "$input_video" -vf "fps=$fps,scale=1920:-1:flags=lanczos" -c:v gif "$output_filename"
 
     echo "GIF created: $output_filename"
 }
