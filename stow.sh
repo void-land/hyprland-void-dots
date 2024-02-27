adf9eaf5b5daa9200d79cc7dfb17198cee92cc90
hesam-init <hesam.init@gmail.com>
2024-02-27 13:40:12 +0330

    feat: new folders structure and stow shell

--- stow.sh
+++ stow.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-dots_dir="dots"
-hyprland_dir="hyprland"
-shortcuts_target_dir="$HOME/.local/share/applications"
+configs_dir="$(pwd)/configs"
+shortcuts_dir="$(pwd)/.shortcuts"
 
 display_help() {
     echo "Usage: [-s | -u] [-h]"
@@ -11,38 +10,64 @@ display_help() {
     echo "  -h   Display this help message"
 }
 
+create_target_dir() {
+    mkdir -p ~/.local/share/applications
+    mkdir -p ~/.config
+}
+
 stow_shortcuts() {
-    mkdir -p $shortcuts_target_dir
-    stow -d $dots_dir -S shortcuts -t $shortcuts_target_dir
+    ln -sfn $shortcuts_dir/* ~/.local/share/applications
 
     echo "Shortcuts stowed successfully!"
 }
 
 stow_dotfiles() {
-    stow $dots_dir
-    stow -d $dots_dir -S zsh -t ~/
-    stow $hyprland_dir
+    for folder in $configs_dir/dots/*; do
+        folder_name=$(basename "$folder")
+
+        if [ ! $folder_name = zsh ]; then
+            ln -sfn $configs_dir/dots/$folder_name ~/.config
+        fi
+    done
 
     echo "Dotfiles stowed successfully!"
 }
 
 unstow_dotfiles() {
-    stow -D $dots_dir
-    stow -D -d $dots_dir -t ~/ zsh
-    stow -D -d $dots_dir -t $shortcuts_target_dir shortcuts
-    stow -D $hyprland_dir
-
     echo "Dotfiles unstowed successfully!"
+}
+
+stow_hyprland() {
+    ln -sfn $configs_dir/hyprland/* ~/.config
+
+    echo "Hyprland stowed successfully!"
+}
+
+unstow_hyprland() {
+    for folder in $configs_dir/hyprland/*; do
+        folder_name=$(basename "$folder")
+        target_folder=~/.config/"$folder_name"
+
+        if [ -e "$target_folder" ]; then
+            rm -rf "$target_folder"
+            echo "Removed: $target_folder"
+        else
+            echo "Not found: $target_folder"
+        fi
+    done
 }
 
 while getopts ":suh" opt; do
     case $opt in
     s)
+        create_target_dir
         stow_dotfiles
         stow_shortcuts
+        stow_hyprland
         ;;
     u)
         unstow_dotfiles
+        unstow_hyprland
         ;;
     *)
         display_help
