c7b83c1d02e240957d3083329f651f249b255156
hesam-init <hesam.init@gmail.com>
2024-02-27 15:13:38 +0330

    feat: create symlinks function

--- stow.sh
+++ stow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 configs_dir="$(pwd)/configs"
+settings_dir="$(pwd)/.settings"
 shortcuts_dir="$(pwd)/.shortcuts"
 
 display_help() {
@@ -10,46 +11,106 @@ display_help() {
     echo "  -h   Display this help message"
 }
 
+create_symlinks() {
+    source_dir=$1
+    target_dir=$2
+
+    if [ ! -d "$source_dir" ] || [ ! -d "$target_dir" ]; then
+        echo "Source or target directory does not exist."
+        return 1
+    fi
+
+    for item in "$source_dir"/* "$source_dir"/.*; do
+        [ -e "$item" ] && [ "$item" != "$source_dir/." ] && [ "$item" != "$source_dir/.." ] && ln -sfn "$item" "$target_dir/"
+    done
+}
+
 create_target_dir() {
     mkdir -p ~/.local/share/applications
     mkdir -p ~/.config
 }
 
 stow_shortcuts() {
-    ln -sfn $shortcuts_dir/* ~/.local/share/applications
+    create_symlinks $shortcuts_dir ~/.local/share/applications
 
     echo "Shortcuts stowed successfully!"
 }
 
-stow_dotfiles() {
-    for folder in $configs_dir/dots/*; do
-        folder_name=$(basename "$folder")
+unstow_shortcuts() {
+    for file in $shortcuts_dir/*; do
+        local file_name=$(basename $file)
+        local target_file=~/.local/share/applications/$file_name
 
-        if [ ! $folder_name = zsh ]; then
-            ln -sfn $configs_dir/dots/$folder_name ~/.config
+        if [ -e $target_file ]; then
+            rm $target_file
+            echo "Removed: $target_file"
+        else
+            echo "Not found: $target_file"
         fi
     done
+}
+
+stow_dotfiles() {
+    create_symlinks $configs_dir/dotfiles ~/.config
+    create_symlinks $configs_dir/zsh ~/
+
+    ln -sfn $settings_dir/mimeapps.list ~/.config
 
     echo "Dotfiles stowed successfully!"
 }
 
 unstow_dotfiles() {
-    echo "Dotfiles unstowed successfully!"
+    for folder in $configs_dir/dotfiles/*; do
+        local folder_name=$(basename $folder)
+        local target_folder=~/.config/$folder_name
+
+        if [ -e $target_folder ]; then
+            rm -rf $target_folder
+            echo "Removed: $target_folder"
+        else
+            echo "Not found: $target_folder"
+        fi
+    done
+
+    for config in $configs_dir/zsh/.*; do
+        if [ -f $config ]; then
+            local file_name=$(basename $config)
+            local target_file=~/$file_name
+
+            if [ -e $target_file ]; then
+                rm $target_file
+                echo "Removed: $target_file"
+            else
+                echo "Not found: $target_file"
+            fi
+
+        elif [ -d "$config" ]; then
+            local folder_name=$(basename $config)
+            local target_folder=~/$folder_name
+
+            if [ -e $target_folder ]; then
+                rm -rf $target_folder
+                echo "Removed: $target_folder"
+            else
+                echo "Not found: $target_folder"
+            fi
+        fi
+    done
 }
 
 stow_hyprland() {
-    ln -sfn $configs_dir/hyprland/* ~/.config
+    create_symlinks $configs_dir/hyprland ~/.config
 
     echo "Hyprland stowed successfully!"
 }
 
 unstow_hyprland() {
     for folder in $configs_dir/hyprland/*; do
-        folder_name=$(basename "$folder")
-        target_folder=~/.config/"$folder_name"
+        folder_name=$(basename $folder)
+        target_folder=~/.config/$folder_name
 
-        if [ -e "$target_folder" ]; then
-            rm -rf "$target_folder"
+        if [ -e $target_folder ]; then
+            rm -rf $target_folder
             echo "Removed: $target_folder"
         else
             echo "Not found: $target_folder"
@@ -67,7 +128,10 @@ while getopts ":suh" opt; do
         ;;
     u)
         unstow_dotfiles
+        unstow_shortcuts
         unstow_hyprland
+
+        echo "All configs stowed successfully !"
         ;;
     *)
         display_help
