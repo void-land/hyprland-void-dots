1a81ea7ac6d06e995bf5ae2966b9a89d47d0672b
hesam-init <hesam.init@gmail.com>
2024-03-01 18:17:19 +0330

    fix: waybar toggle script

--- configs/hyprland/hypr/scripts/waybar-toggle.sh
+++ configs/hyprland/hypr/scripts/waybar-toggle.sh
@@ -6,9 +6,7 @@ WAYBAR_PID=$(pgrep -x waybar)
 
 if [ -n "$WAYBAR_PID" ]; then
     pkill waybar
-fi
-
-if [ $WAYBAR_DEV_MODE = true ]; then
+elif [ $WAYBAR_DEV_MODE = true ]; then
     GTK_DEBUG=interactive exec $WAYBAR_LAUNCHER
 else
     exec $WAYBAR_LAUNCHER
