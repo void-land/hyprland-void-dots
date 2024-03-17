8cd9aa217c279f666f80a6557c4ef4cbfcc7c2b2
hesam-init <hesam.init@gmail.com>
2024-03-17 17:59:42 +0330

    feat: pipewire autostart shell

---
+++ configs/hyprland/environments/pipewire.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+check_and_start() {
+    if ! pgrep -x "$1" >/dev/null; then
+        echo "Starting $1..."
+        sleep 1 && $2 &
+    else
+        echo "$1 is already running."
+    fi
+}
+
+check_and_start "pipewire" "pipewire"
+check_and_start "pipewire-pulse" "/usr/bin/pipewire-pulse"
+check_and_start "wireplumber" "/usr/bin/wireplumber"
