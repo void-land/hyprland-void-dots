9315da46e4502b645780015d0c9581e27e98f476
hesam-init <hesam.init@gmail.com>
2024-03-18 19:14:26 +0330

    fix: steam os shell file

--- configs/sh/.steam-os/main.sh
+++ configs/sh/.steam-os/main.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 run_services() {
-    exec dbus-update-activation-environment --all
-    exec ~/.config/scripts/pipewire.sh
+    exec $HOME/.config/scripts/pipewire.sh
 }
 
 run_steamos() {
-    local width=1600
-    local height=900
-    local refresh_rate=75
+    local WIDTH=1600
+    local HEIGHT=900
+    local REFRESH_RATE=75
 
-    STEAM_MULTIPLE_XWAYLANDS=1 gamescope -W "$width" -H "$height" -r "$refresh_rate" -e --xwayland-count 2 --adaptive-sync -- steam -gamepadui -steamdeck
+    STEAM_MULTIPLE_XWAYLANDS=1 gamescope -W $WIDTH -H $HEIGHT -r $REFRESH_RATE -e --xwayland-count 2 --adaptive-sync -- steam -gamepadui -steamdeck
 }
 
 run_services
