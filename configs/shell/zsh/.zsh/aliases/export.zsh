33bb00fb5495e37834bafa6a1307ba5501a26ef3
hesam-init <hesam.init@gmail.com>
2024-03-31 21:16:59 +0330

    chore: alias folder rename

--- configs/shell/zsh/.zsh/aliases/export.zsh
+++ configs/shell/zsh/.zsh/aliases/export.zsh
@@ -1,4 +1,4 @@
 source ~/.zsh/aliases/global/main.zsh
-source ~/.zsh/aliases/dir/main.zsh
+source ~/.zsh/aliases/directories/main.zsh
 source ~/.zsh/aliases/git/main.zsh
-source ~/.zsh/aliases/net/main.zsh
+source ~/.zsh/aliases/network/main.zsh
