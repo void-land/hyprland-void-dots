7da6ca52d0972fc6f53fc3dd30ecc4c9f66a8f4d
hesam-init <hesam.init@gmail.com>
2024-03-31 18:29:11 +0330

    chore: gtk theme

--- configs/shell/zsh/.gamescope/steam.sh
+++ configs/shell/zsh/.gamescope/steam.sh
@@ -4,7 +4,7 @@ source ~/.gamescope/utils/main.sh
 source ~/.gamescope/configs.sh
 
 run_os() {
-    MANGOHUD=1 STEAM_MULTIPLE_XWAYLANDS=1 gamescope -s 0.6 -e --xwayland-count 2 --adaptive-sync -F fsr -W $WIDTH -H $HEIGHT -r $REFRESH_RATE -- steam -gamepadui -steamdeck
+    MANGOHUD=1 STEAM_MULTIPLE_XWAYLANDS=1 gamescope -s 0.4 -e --xwayland-count 2 --adaptive-sync -F fsr -W $WIDTH -H $HEIGHT -r $REFRESH_RATE -- steam -gamepadui
 }
 
 presetup
