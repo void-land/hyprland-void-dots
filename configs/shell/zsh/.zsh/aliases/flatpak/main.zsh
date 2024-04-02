69644e822543113cf33225a5290880f220f28cb9
hesam-init <hesam.init@gmail.com>
2024-04-02 19:51:00 +0330

    feat: flatpak install alias

--- configs/shell/zsh/.zsh/aliases/flatpak/main.zsh
+++ configs/shell/zsh/.zsh/aliases/flatpak/main.zsh
@@ -1,4 +1,5 @@
 alias fclean="flatpak uninstall --unused --delete-data"
+alias fpkg="flatpak install"
 alias fup="flatpak update"
 alias fsearch="flatpak search"
 alias fdir="cd $HOME/.var/app && ls"
