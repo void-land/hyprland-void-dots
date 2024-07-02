66c17b4ac5361d7388696283be545e665ec2a311
hesam-init <hesam.init@gmail.com>
2024-07-02 19:33:02 +0330

    feat: disconnected wifi status

--- hypr-configs/hyprland/eww/scripts/cava_pipe.sh
+++ hypr-configs/hyprland/eww/scripts/cava_pipe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-confPath="$HOME/.config/eww/carbonmonoxide/scripts/cava_config"
 
-# Main
-cava -p $confPath | while read -r line; do echo $line| sed -e 's/;/,/g'; done | while read -r line; do echo "["`echo ${line/%?/}`"]"; done
+confPath="$HOME/.config/eww/scripts/cava_config"
+
+cava -p $confPath | while read -r line; do echo $line | sed -e 's/;/,/g'; done | while read -r line; do echo "["$(echo ${line/%?/})"]"; done
